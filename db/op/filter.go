b5866197eb6fbb5b7a3a2e4d36e29e532240bb9e
xixiwang <fewzh@mail.scut.edu.cn>
2023-11-27 15:45:31 +0800

    [fix] fix clause

--- db/op/filter.go
+++ db/op/filter.go
@@ -4,48 +4,48 @@ type CondType uint8
 
 type Condition struct {
 	Query string
-	Value []any
+	Value any
 }
 
 // Eq 等于
 func Eq(field string, value any) *Condition {
-	return &Condition{Query: field + " = ?", Value: []any{value}}
+	return &Condition{Query: field + " = ?", Value: value}
 }
 
 // Lt 小于
 func Lt(field string, value any) *Condition {
-	return &Condition{Query: field + " < ?", Value: []any{value}}
+	return &Condition{Query: field + " < ?", Value: value}
 }
 
 // Le 小于等于
 func Le(field string, value any) *Condition {
-	return &Condition{Query: field + " <= ?", Value: []any{value}}
+	return &Condition{Query: field + " <= ?", Value: value}
 }
 
 // Gt 大于
 func Gt(field string, value any) *Condition {
-	return &Condition{Query: field + " > ?", Value: []any{value}}
+	return &Condition{Query: field + " > ?", Value: value}
 }
 
 // Ge 大于等于
 func Ge(field string, value any) *Condition {
-	return &Condition{Query: field + " >= ?", Value: []any{value}}
+	return &Condition{Query: field + " >= ?", Value: value}
 }
 
 // Ne 不等于
 func Ne(field string, value any) *Condition {
-	return &Condition{Query: field + " <> ?", Value: []any{value}}
+	return &Condition{Query: field + " <> ?", Value: value}
 }
 
 // Like 模糊查询
 func Like(field string, value string) *Condition {
-	return &Condition{Query: field + " like ?", Value: []any{"%" + value + "%"}}
+	return &Condition{Query: field + " like ?", Value: "%" + value + "%"}
 }
 
 func In(field string, value any) *Condition {
-	return &Condition{Query: field + " in ?", Value: []any{value}}
+	return &Condition{Query: field + " in ?", Value: value}
 }
 
 func NotIn(field string, value any) *Condition {
-	return &Condition{Query: field + " not in ?", Value: []any{value}}
+	return &Condition{Query: field + " not in ?", Value: value}
 }
